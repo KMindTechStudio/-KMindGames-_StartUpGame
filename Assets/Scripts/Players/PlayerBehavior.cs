c02e5a42d9ac44752f91810b9609668c83e8bd4d
Chu Quý <quyngoquyham@gmail.com>
2025-04-15 22:58:14 +0700

    15/4/2025

---
+++ Assets/Scripts/Players/PlayerBehavior.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameObject player;
+    private float dashCooldown = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
